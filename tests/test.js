e9652a59a963220ba902cf5c20af9e1332949fd9
kimpure <gimleni593@gmail.com>
2025-01-18 12:47:07 +0000

    format backup

---
+++ tests/test.js
@@ -0,0 +1,14 @@
+const string = 'string';
+const number = 0;
+const boolean = true;
+const array = [ 100, 0, 1, 2, 3, 9, 99, null ];
+const object = { z: '1', c: '2', 0: 'q', true: 'false' };
+
+// console.log(string, number, boolean, array, object, null, undefined);
+// console.log(object);
+// console.log(array);
+
+// console.error('tset');
+throw console.error('test');
+
+
